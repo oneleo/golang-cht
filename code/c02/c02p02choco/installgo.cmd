7cc5c57cdbee12ccb1feb5751ad2a3cce793e50c
oneleo <oneleo760823@gmail.com>
2021-02-05 17:42:47 +0800

    Update

--- code/c02/c02p02choco/installgo.cmd
+++ code/c02/c02p02choco/installgo.cmd
@@ -1,5 +1,5 @@
 :: Run as administrator through CMD.exe
 @echo off
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-choco upgrade -y --params="'/NoShellIntegration'" git.install golang vscode vscode-go mingw
+choco install -y --params="'/NoShellIntegration'" git.install golang vscode vscode-go mingw
 pause
