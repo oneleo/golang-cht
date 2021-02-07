cd0345d49511837b8495c56cfc7723f7d528d98a
oneleo <oneleo760823@gmail.com>
2021-02-07 10:43:22 +0800

    Update

---
+++ code/c02/c02p04gopath/src/mymain/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"mypack"
+)
+
+func main() {
+	//fmt.Println("mypack.var1 =", mypack.var1)
+	fmt.Println("mypack.Var2 =", mypack.Var2)
+	//mypack.myfunc1(4)
+	mypack.Myfunc2(3)
+}
---
+++ code/c02/c02p04gopath/src/mypack/pack.go
@@ -0,0 +1,12 @@
+package mypack
+
+import "fmt"
+
+var var1, Var2 int
+
+func myfunc1(input int) {
+	fmt.Println("myfunc1 input =", input)
+}
+func Myfunc2(input int) {
+	fmt.Println("Myfunc2 input =", input)
+}
