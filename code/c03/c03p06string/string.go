c72e61cfa881e35f142c4fa3b44d19ad72ee17ea
oneleo <oneleo760823@gmail.com>
2021-02-24 19:30:04 +0800

    Update string.go

--- code/c03/c03p06string/string.go
+++ code/c03/c03p06string/string.go
@@ -8,55 +8,31 @@ import (
 )
 
 func main() {
-	s := "I'm \"Gopher\"!"
-	for k, v := range s {
-		fmt.Printf("k：%d,v：%c == %d\n", k, v, v)
+	w1, w2, w3 := "Hi,", "世界！❤", "a,p,p"
+	for i, v := range w1 {
+		fmt.Printf("i：%d, v：%c == 0x%X\n", i, v, v)
 	}
+	fmt.Printf("「%s」\t\t=> Byte 數：%d\t字數：%d\n", w1, len(w1), utf8.RuneCountInString(w1))
+	fmt.Printf("「%s」\t=> Byte 數：%d\t字數：%d\n", w2, len(w2), utf8.RuneCountInString(w2))
+	res1 := strings.Split(w3, ",")           // 拆解
+	res2 := strings.ReplaceAll(w3, ",", "&") // 取代
+	res3 := strings.Index(w3, "p")           // 從左向右查詢
+	res4 := strings.LastIndex(w3, "p")       // 從右向左查詢
+	res5 := strings.LastIndex(w3, "b")       // 查詢失敗（= -1）
+	fmt.Printf("拆解：%v\n取代：%v\n從左向右查詢：%v\n從右向左查詢：%v\n查詢失敗：%v\n", res1, res2, res3, res4, res5)
 
-	i := 10
-	s1 := strconv.FormatInt(int64(i), 10)
-	s2 := strconv.Itoa(i)
-	s3 := fmt.Sprintf("%d", i)
-	fmt.Printf("%v, %v, %v\n", s1, s2, s3)
-
-	w1 := "Aikido"
-	fmt.Printf(w1 + "\n")
-	fmt.Printf("Number of bytes %d\n", len(w1))
-	fmt.Printf("Number of runes %d\n", utf8.RuneCountInString(w1))
-	w2 := "合氣道"
-	fmt.Printf(w2 + "\n")
-	fmt.Printf("Number of bytes %d\n", len(w2))
-	fmt.Printf("Number of runes %d\n", utf8.RuneCountInString(w2))
-
-	//Case 1 s contains sep. Will output slice of length 3
-	res := strings.Split("ab$cd$ef", "$")
-	fmt.Println(res)
-	//Case 2 s doesn't contain sep. Will output slice of length 1
-	res = strings.Split("ab$cd$ef", "-")
-	fmt.Println(res)
-	//Case 3 sep is empty. Will output slice of length 8
-	res = strings.Split("ab$cd$ef", "")
-	fmt.Println(res)
-	//Case 4 both s and sep are empty. Will output empty slice
-	res = strings.Split("", "")
-	fmt.Println(res)
-	res2 := strings.ReplaceAll("abcdabxyabr", "a", "")
-	fmt.Println(res2)
-
-	value := "cat dog"
-	// Get index of this substring.
-	result := strings.Index(value, "dog")
-	fmt.Println(result)
-
-	input := "one two one"
-	// Get last index, searching from right to left.
-	ii := strings.LastIndex(input, "one")
-	fmt.Println(ii)
-
-	value1 := "rainbow"
-	value2 := "the rainbow"
-	// See if value1 is found in value2.
-	if strings.Index(value2, value1) != -1 {
-		fmt.Println("FOUND")
+	i, j := 10, "10"
+	str1 := strconv.Itoa(i) // 數字轉字串
+	str2 := strconv.FormatInt(int64(i), 10)
+	str3 := fmt.Sprintf("%d", i)
+	fmt.Printf("數字轉字串：\"%v\", \"%v\", \"%v\"\n", str1, str2, str3)
+	if i, err := strconv.Atoi(j); err == nil {
+		fmt.Printf("字串轉數字：%v, ", i)
+	}
+	if i, err := strconv.ParseInt(j, 10, 64); err == nil {
+		fmt.Printf("%v, ", i)
+	}
+	if _, err := fmt.Sscanf(j, "%d", &i); err == nil {
+		fmt.Printf("%v\n", i)
 	}
 }
