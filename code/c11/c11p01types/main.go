9e34a20512fbe46be8c17162de92e945eb74768a
oneleo <oneleo760823@gmail.com>
2021-08-04 00:09:48 +0800

    Modify Slide 11

---
+++ code/c11/c11p01types/main.go
@@ -0,0 +1,22 @@
+// START OMIT
+package main
+
+import "fmt"
+
+type Number int
+
+func (n *Number) Display() {
+	fmt.Println(*n)
+}
+
+func (n *Number) Double() {
+	*n *= 2
+}
+
+func main() {
+	number := Number(4)
+	number.Double()
+	number.Display()
+}
+
+// END OMIT
