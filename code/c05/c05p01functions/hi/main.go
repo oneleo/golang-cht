fd6c027b43244664191bdf48102a0247c91c5f73
oneleo <oneleo760823@gmail.com>
2021-08-03 17:02:43 +0800

    New code

---
+++ code/c05/c05p01functions/hi/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/oneleo/golang-cht/code/c05/c05p01functions/greeting"
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+}
+
+// PS C:\Users\irara\go\src\github.com\oneleo\golang-cht> go run github.com/oneleo/golang-cht/code/c05/c05p01functions/hi
+// PS C:\Users\irara\go\src\github.com\oneleo\golang-cht> go run ./code/c05/c05p01functions/hi
