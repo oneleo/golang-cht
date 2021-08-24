ef86ef804f9e194127a55a402788d64c710fb7fa
oneleo <oneleo760823@gmail.com>
2021-08-24 14:29:53 +0800

    Modify some code

--- code/c03/c03p11pointer/pointer.go
+++ code/c03/c03p11pointer/pointer.go
@@ -9,11 +9,12 @@ func main() {
 		p1 *int8 = &v1
 		p2 *int8 = &v2
 	)
-	fmt.Printf("1、存放 v1（= %v）的位址為：%v\t存放 v2（= %v）的位址為：%v\n", v1, p1, v2, p2)
+	fmt.Printf("1、存放 v1（= %v）的位址為：%v\n存放 v2（= %v）的位址為：%v\n\n", v1, p1, v2, p2)
 	// * 為取值運算子
-	fmt.Printf("2、回推 p1 位址對映的值為：%v\t\t\t回推 p2 位址對映的值為：%v\n", *p1, *p2)
+	fmt.Printf("2、回推 p1 位址對映的值為：%v\n回推 p2 位址對映的值為：%v\n\n", *p1, *p2)
 	// 若嘗試修改 *p1、*p2 的值
 	*p1, *p2 = 10, 20
-	fmt.Printf("3、修改 *p1（= %v）及 *p2（= %v）後，v1 現在的值為：%v\tv2 現在的值為：%v\n", *p1, *p2, v1, v2)
+	fmt.Printf("3、修改 *p1（to %v）及 *p2（to %v）後，\n", *p1, *p2)
+	fmt.Printf("v1 現在的值為：%v\tv2 現在的值為：%v\n", v1, v2)
 	// END OMIT
 }
--- tmp/golang-cht-part-01.cmd
+++ tmp/golang-cht-part-01.cmd
@@ -6,31 +6,31 @@
 
 :: Set Visual Studio Code Environment for Golang
 ::"%ProgramFiles%\Microsoft VS Code\bin\code" --force --install-extension "golang.go"
-"%ProgramFiles%\Go\bin\go.exe" get "github.com/acroca/go-symbols"
-"%ProgramFiles%\Go\bin\go.exe" get "github.com/davidrjenni/reftools/cmd/fillstruct"
-"%ProgramFiles%\Go\bin\go.exe" get "github.com/haya14busa/goplay/cmd/goplay"
+"%ProgramFiles%\Go\bin\go.exe" get -u "github.com/acroca/go-symbols"
+"%ProgramFiles%\Go\bin\go.exe" get -u "github.com/davidrjenni/reftools/cmd/fillstruct"
+"%ProgramFiles%\Go\bin\go.exe" get -u "github.com/haya14busa/goplay/cmd/goplay"
 :: Install two versions of gocode (one as gocode-gomod)
-"%ProgramFiles%\Go\bin\go.exe" get "github.com/stamblerre/gocode"
+"%ProgramFiles%\Go\bin\go.exe" get -u "github.com/stamblerre/gocode"
 move /y "%USERPROFILE%\go\bin\gocode.exe" "%USERPROFILE%\go\bin\gocode-gomod.exe"
-"%ProgramFiles%\Go\bin\go.exe" get "github.com/mdempsky/gocode"
-"%ProgramFiles%\Go\bin\go.exe" get "github.com/sqs/goreturns"
-"%ProgramFiles%\Go\bin\go.exe" get "github.com/uudashr/gopkgs/v2/cmd/gopkgs"
-"%ProgramFiles%\Go\bin\go.exe" get "github.com/zmb3/gogetdoc"
-"%ProgramFiles%\Go\bin\go.exe" get "honnef.co/go/tools/..."
-"%ProgramFiles%\Go\bin\go.exe" get "golang.org/x/tools/cmd/gorename"
+"%ProgramFiles%\Go\bin\go.exe" get -u "github.com/mdempsky/gocode"
+"%ProgramFiles%\Go\bin\go.exe" get -u "github.com/sqs/goreturns"
+"%ProgramFiles%\Go\bin\go.exe" get -u "github.com/uudashr/gopkgs/v2/cmd/gopkgs"
+"%ProgramFiles%\Go\bin\go.exe" get -u "github.com/zmb3/gogetdoc"
+"%ProgramFiles%\Go\bin\go.exe" get -u "honnef.co/go/tools/..."
+"%ProgramFiles%\Go\bin\go.exe" get -u "golang.org/x/tools/cmd/gorename"
 :: https://github.com/golang/go/issues/45732
-"%ProgramFiles%\Go\bin\go.exe" get "golang.org/x/tools/gopls"
-"%ProgramFiles%\Go\bin\go.exe" get "github.com/cweill/gotests/..."
-"%ProgramFiles%\Go\bin\go.exe" get "github.com/rogpeppe/godef"
-"%ProgramFiles%\Go\bin\go.exe" get "github.com/ramya-rao-a/go-outline"
-"%ProgramFiles%\Go\bin\go.exe" get "github.com/go-delve/delve/cmd/dlv@master"
+"%ProgramFiles%\Go\bin\go.exe" get -u "golang.org/x/tools/gopls"
+"%ProgramFiles%\Go\bin\go.exe" get -u "github.com/cweill/gotests/..."
+"%ProgramFiles%\Go\bin\go.exe" get -u "github.com/rogpeppe/godef"
+"%ProgramFiles%\Go\bin\go.exe" get -u "github.com/ramya-rao-a/go-outline"
+"%ProgramFiles%\Go\bin\go.exe" get -u "github.com/go-delve/delve/cmd/dlv@master"
 copy /v /y "%USERPROFILE%\go\bin\dlv.exe" "%USERPROFILE%\go\bin\dlv-dap.exe"
-"%ProgramFiles%\Go\bin\go.exe" get "github.com/go-delve/delve/cmd/dlv"
+"%ProgramFiles%\Go\bin\go.exe" get -u "github.com/go-delve/delve/cmd/dlv"
 
 :: Golang Present for CHT
 "%ProgramFiles%\Git\cmd\git.exe" clone "https://github.com/oneleo/golang-cht.git" "%USERPROFILE%\go\src\github.com\oneleo\golang-cht"
 "%ProgramFiles%\Git\cmd\git.exe" clone "https://github.com/golang/tools.git" "%USERPROFILE%\go\src\golang.org\x\tools"
-"%ProgramFiles%\Go\bin\go.exe" get "golang.org/x/tools/cmd/present"
+"%ProgramFiles%\Go\bin\go.exe" get -u "golang.org/x/tools/cmd/present"
 cd /d "%USERPROFILE%\go\src\github.com\oneleo\golang-cht"
 start microsoft-edge:http://127.0.0.1:3999
 "%USERPROFILE%\go\bin\present.exe" -base "%USERPROFILE%\go\src\golang.org\x\tools\cmd\present" -notes
